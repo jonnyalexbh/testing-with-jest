8efeedb657b188ae5b601b33d389b909f0088e4b
jonnyalexbh <jonnyalex.bh@gmail.com>
2019-11-04 20:35:48 -0500

    reject y resolve with jest

--- __test__/promise.test.js
+++ __test__/promise.test.js
@@ -1,11 +1,17 @@
 import { getDataFromApi } from '../promise';
 
 describe('testing promises', () => {
-  test('Realizando una peticion a una api', done => {
+  test('making a request to an api', done => {
     const api = 'https://rickandmortyapi.com/api/character/';
     getDataFromApi(api).then(data => {
       expect(data.results.length).toBeGreaterThan(0);
       done();
     });
+  });
+  test('solve a Hello!', () => {
+    return expect(Promise.resolve('Hola!')).resolves.toBe('Hola!');
+  });
+  test('reject with an error', () => {
+    return expect(Promise.reject('Errror')).rejects.toBe('Errror');
   });
 });
