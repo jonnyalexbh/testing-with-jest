3c0fdbc9be14cbacac5fb943bc4f9173cbb93055
jonnyalexbh <jonnyalex.bh@gmail.com>
2020-09-11 10:33:20 -0500

    mock functions

--- __test__/mock-fn.test.js
+++ __test__/mock-fn.test.js
@@ -1,6 +1,7 @@
 import * as math from '../maths';
 
 math.sumar = jest.fn().mockReturnValue(3);
+math.multiplicar = jest.fn();
 
 describe('mathematical calculations jest fn', () => {
   it('sums test', () => {
@@ -9,4 +10,13 @@ describe('mathematical calculations jest fn', () => {
     expect(math.sumar).toHaveBeenCalledWith(2, 1);
     expect(math.sumar).toHaveBeenCalledTimes(1);
   })
+
+  it('multiplication test', () => {
+    math.multiplicar.mockReturnValue(8)
+    expect(math.multiplicar(4, 2)).toBe(8);
+    expect(math.multiplicar).toHaveBeenCalled();
+    expect(math.multiplicar).toHaveBeenCalledWith(4, 2);
+    expect(math.multiplicar).toHaveBeenCalledTimes(1);
+  })
 })
+
